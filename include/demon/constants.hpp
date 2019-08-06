70dbe957a9c413ef560a5123b1fa1d9f62570b23
Sho Hirose <sho.hirose@utexas.edu>
2019-08-05 20:15:54 -0500

    style: translate comments written in Japanese into English in demon/constants.hpp.

--- include/demon/constants.hpp
+++ include/demon/constants.hpp
@@ -1,24 +1,26 @@
 #ifndef DEMON_CONSTANTS_HPP
 #define DEMON_CONSTANTS_HPP
 
-constexpr int NP = 300;        //粒子数
-constexpr int BTD = 9;         //完全平衡木の深さ, = ceil(log2(NP))
+constexpr int NP = 300;  // Number of particles
+constexpr int BTD = 9;   // Depth of the balanced tree = ceil(log2(NP))
+// Parameters for the balanced tree
 constexpr auto Q = 512 - 300;  // = pow(2,BTD) - NP
 constexpr auto P = (NP - Q) / 2;
-
-constexpr double RDISK = 0.02;   // radius of disk
-constexpr double PREST = 0.95;   //粒子同士の反発係数
-constexpr double WREST = 1.0;    //壁との反発係数
-constexpr double GRAVITY = 1.0;  //規格化された重力加速度
-constexpr double XMIN = -1.0;    //左の壁の位置
-constexpr double XMAX = 1.0;     //右の壁の位置
-constexpr double YMIN = 0.0;     //底面の位置
-constexpr double YMAX = 1.0;     //セルの最高点の位置
-constexpr double U = 0.149;      //床面の振動する速度
-constexpr double V0 = 1.0;       //初期条件での速度分布の標準偏差
-constexpr int NCELLX = 32;       // x方向のセルの分割数
-constexpr int NCELLY = 12;       // y方向のセルの分割数
-constexpr double TEND = 20.0;    //シミュレーション終了時刻
-constexpr double EPS = 0.000001;
+constexpr double RDISK = 0.02;  // Radius of disk
+constexpr double PREST = 0.95;  // Coefficient of restitution between particles
+// Coefficient of restitution between a particle and a wall
+constexpr double WREST = 1.0;
+constexpr double GRAVITY = 1.0;  // Normalized gravity acceleration
+constexpr double XMIN = -1.0;    // X of the left wall
+constexpr double XMAX = 1.0;     // X of the right wall
+constexpr double YMIN = 0.0;     // Y of the bottom wall
+constexpr double YMAX = 1.0;     // Y of the top wall
+constexpr double U = 0.149;      // Oscillation vecolity of the bottom wall
+// Standard deviation of velocity distribution at the initial state
+constexpr double V0 = 1.0;
+constexpr int NCELLX = 32;        // Number of cells in x direction
+constexpr int NCELLY = 12;        // Number of cells in y direction
+constexpr double TEND = 20.0;     // End time of simulation
+constexpr double EPS = 0.000001;  // Tolerance
 
 #endif  // DEMON_CONSTANTS_HPP
